331544032cf74a9e75e4fcfab00e74782f829f73
jgoede <jgoede@siue.edu>
2019-07-20 18:48:05 -0500

    implemented selecting questionaire by sex

--- everscreen/lib/main.dart
+++ everscreen/lib/main.dart
@@ -50,7 +50,7 @@ class Landing extends StatelessWidget {
             onPressed: () {
               Navigator.push(
               ctxt,
-              new MaterialPageRoute(builder: (ctxt) => new Login()),
+              new MaterialPageRoute(builder: (ctxt) => new Manager()),//TODO change back to skip
             );
           },
           child: Text('Continue without logging in')
@@ -107,7 +107,12 @@ class Login extends StatelessWidget {
           ),
           RaisedButton(
             child: Text('Login'),
-            onPressed: (){},
+            onPressed: (){
+              Navigator.push(
+              ctxt,
+              new MaterialPageRoute(builder: (ctxt) => new Manager()),
+              );
+            }
           ),
           Row(
             children:
@@ -234,6 +239,75 @@ Widget build (BuildContext ctxt) {
 }
 }
 
+class Skip extends StatefulWidget {
+    
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SkipState();
+  }
+
+}
+class _SkipState extends  State<StatefulWidget> {
+  String dropdownValue = 'Male';
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Select gender and age"),
+      ),
+      body: Column(
+        children: [
+              Row(
+                  children: [
+                      Text('Age: '),
+                      new Flexible(
+                        child: new TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Enter your age in full years'//TODO: change this to birthdate here and in sign up
+                          )
+                        )
+                      )
+                  ]
+              ),
+              Row(
+                children: [
+                  Text('Sex: '),
+                  
+                DropdownButton<String>(
+                  value: dropdownValue,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['Male','Female']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                    ]
+                  ),
+                  Row(
+                      children: [
+                          new RaisedButton(
+                              onPressed: () {
+                                
+                                 
+                                  launchMetrics(dropdownValue, ctxt);
+                          },
+                          child: Text('Sign Up!')
+                      )
+                  ]
+              )
+          ]
+      )
+    );
+  }
+}
 
 class Signup extends StatefulWidget {
     
@@ -304,10 +378,7 @@ class _SignupState extends  State<StatefulWidget> {
                       children: [
                           new RaisedButton(
                               onPressed: () {
-                                  Navigator.push(
-                                  ctxt,
-                                  new MaterialPageRoute(builder: (ctxt) => new Metrics()),
-                              );
+                                  launchMetrics(dropdownValue, ctxt);                          
                           },
                           child: Text('Sign Up!')
                       )
@@ -319,15 +390,72 @@ class _SignupState extends  State<StatefulWidget> {
   }
 }
 
+class MetricsF extends StatefulWidget {
+    
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MetricsFState();
+  }
+}
 
-class Metrics extends StatelessWidget {
+class _MetricsFState extends State<StatefulWidget>
+{
+  //state variables go here
+
   @override
   Widget build(BuildContext ctxt) {
-    return new MaterialApp(
-      home: new Landing(),
-    );
+    return new MaterialApp( 
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text('Metric Gathering'),
+      ),
+        body: Column(
+          children:
+            [
+              Text("This is a placeholder"),
+              new Flexible()
+              
+            ]
+          )
+        )
+      );
   }
-} 
+}
+
+class MetricsM extends StatefulWidget {
+    
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MetricsMState();
+  }
+}
+
+class _MetricsMState extends State<StatefulWidget>
+{
+  //state variables go here
+
+  @override
+  Widget build(BuildContext ctxt) {
+    return new MaterialApp( 
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text('Metric Gathering'),
+      ),
+        body: Column(
+          children:
+            [
+              Text("This is a placeholder"),
+              new Flexible()
+              
+            ]
+          )
+        )
+      );
+  }
+}
+
 class Current extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
@@ -337,13 +465,123 @@ class Current extends StatelessWidget {
   }
 } 
 class Info extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctxt) {
-    return new MaterialApp(
-      home: new Landing(),
-    );
+// Test Subject Data
+var name = 'John Doe';
+var race = 'Asian';
+int PSA = 5;
+int age = 22;
+int systolic = 120;
+int diastolic = 80;
+bool cervicalCancerRisk = true;
+bool familyHistoryColorectal = true;
+bool heartDiseaseRisk = true;
+bool highBloodPressure = true;
+bool highRiskBP = true;
+bool highRiskCholestrol = true;
+bool puberty = true;
+// date birthdate = July 20, 1994 at 12:00:00 AM UTC-5;
+// date lastBloodPressureScreening = July 6, 2016 at 12:00:00 AM UTC-5;
+// date lastColonoscopyScreening = July 20, 2016 at 12:00:00 AM UTC-5;
+// date lastInfluenzaScreening = July 20, 2017 at 12:00:00 AM UTC-5;
+// date lastProstateScreening = July 20, 1997 at 12:00:00 AM UTC-5;
+@override
+Widget build (BuildContext ctxt) {
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text("Your Information"),
+    ),
+    body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+            Row(
+                children: [
+                    Text('Name: '),
+                    new Flexible(
+                      child: new Text('$name')
+                    )
+                ]
+            ),
+            Row(
+                children: [
+                    Text('Age: '),
+                    new Flexible(
+                      child: new Text('$age')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('Race: '),
+                    new Flexible(
+                      child: new Text('$race')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('Birthdate '),
+                    new Flexible(
+                      child: new Text('birthdate')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('Systolic / Diastolic: '),
+                    new Flexible(
+                      child: new Text('$systolic' + ' / ' + '$diastolic')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('High Blood Pressure: '),
+                    new Flexible(
+                      child: new Text('$highBloodPressure')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('High Blood Pressure Risk: '),
+                    new Flexible(
+                      child: new Text('$highRiskBP')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('High Cholesterol Risk: '),
+                    new Flexible(
+                      child: new Text('$highRiskCholestrol')
+                    )
+                ]
+            ),
+             Row(
+                children: [
+                    Text('Heart Disease Risk: '),
+                    new Flexible(
+                      child: new Text('$heartDiseaseRisk')
+                    )
+                ]
+            )
+        ]
+    )
+  );
+}
+}
+void launchMetrics(String sex, BuildContext ctxt)
+{
+  if(sex == 'male')
+  {
+    Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new MetricsM()));
   }
-} 
+  else
+  {
+    Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new MetricsF()));
+  }
+}
+
 class Schedule extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
