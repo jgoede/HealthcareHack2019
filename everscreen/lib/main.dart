a94522d94d34ee14675c36a763f20d856a568ea3
jgoede <jgoede@siue.edu>
2019-07-20 13:52:53 -0500

    Fixed flex issue

--- everscreen/lib/main.dart
+++ everscreen/lib/main.dart
@@ -41,7 +41,7 @@ class Landing extends StatelessWidget {
             child: Text('Login')
           ),
             
-          Row(
+          Column(
             children: [
 
             new RaisedButton(
@@ -73,7 +73,138 @@ class Login extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Login"),
       ),
-      body: new Text(""),
+      body: Column(
+        children:
+        [
+          Row(
+            children: [
+            Text('Username'),
+            new Flexible(
+              child: new 
+              TextField(
+                decoration: 
+                  InputDecoration(
+                  labelText: 'Enter Username')
+              )
+            )
+            ]
+          ),
+          Row(
+            children: [
+            Text('Password'),
+            new Flexible(
+              child: new 
+              TextField(
+                decoration: 
+                  InputDecoration(
+                  labelText: 'Enter Password')
+              )
+            )
+            ]
+          ),
+          RaisedButton(
+            child: Text('Login'),
+            onPressed: (){},
+          ),
+          Row(
+            children:
+            [
+              Text('Don\'t have an account?'),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                  ctxt,
+                  new MaterialPageRoute(builder: (ctxt) => new Signup()),
+                  );
+                },
+                 child: Container(
+
+                  child: Text(
+                    'CLICK HERE',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              )
+            ]
+          )
+        ]
+        
+        
+        
+        
+        
+      )
+    );
+  }
+}
+class Signup extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Sign Up"),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+              Row(
+                  children: [
+                      Text('Name: '),
+                      new Flexible( 
+                        child: new TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Enter your full name'
+                          )
+                        )
+                      )
+                  ]
+              ),
+              Row(
+                  children: [
+                      Text('Age: '),
+                      new Flexible(
+                        child: new TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Enter your age in full years'
+                          )
+                        )
+                      )
+                  ]
+              ),
+              Row(
+                  children: [
+                      Text('Sex: '),
+                      new Flexible(child: new TextFormField(
+                          decoration: InputDecoration(
+                              labelText: ' '
+                          )
+                        )
+                      )
+                  ]
+              ),
+              Row(
+                  children: [
+                      new RaisedButton(
+                          onPressed: () {
+                              Navigator.push(
+                                  ctxt,
+                                  new MaterialPageRoute(builder: (ctxt) => new Metrics()),
+                              );
+                          },
+                          child: Text('Sign Up!')
+                      )
+                  ]
+              )
+          ]
+      )
+    );
+  }
+}
+class Metrics extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new MaterialApp(
+      home: new Landing(),
     );
   }
 }
